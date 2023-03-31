77d347f0eeb9bcb1290035efb47925edc365f0f1
cdcaikl <47206883+cdcaikl@users.noreply.github.com>
2023-03-31 14:50:55 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  facebook: 'sjbcar',
-  instagram: 'sjb_china',
-  telegram: '@sjbcar',
+  twitter: '',
+  github: '',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
